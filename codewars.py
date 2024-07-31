e61b06dc0d7152d95258f1616678b69942edb47d
Tsihar <tsitavets.ihar@gmail.com>
2024-07-31 23:45:46 +0300

    codewars задачки

--- codewars.py
+++ codewars.py
@@ -71,8 +71,10 @@ def validate_pin(pin):
 
 print(validate_pin("098765"))
 
+
 def validate_pin_solution(pin):
     return len(pin) in (4, 6) and pin.isdigit()
+
 
 def stray(arr):
     return min(arr, key=arr.count)
@@ -80,6 +82,93 @@ def stray(arr):
     #     if arr.count(i) == 1:
     #         return i
 
+
 # Когда мы вызываем min(arr, key=arr.count), функция min() найдет элемент,
 # для которого arr.count(x) минимален, то есть элемент с минимальным количеством вхождений.
 print(stray([1, 1, 1, 3]))
+
+
+def solution(string):
+    spisok = list(string)
+    spis = spisok.sort(reverse=True)
+    return spis
+
+
+print(solution('hello'))
+
+
+def to_alternating_case(string):
+    new_string = ''
+    for i in string:
+        if i.islower():
+            new_string += i.upper()
+        elif i.isupper():
+            new_string += i.lower()
+
+    return new_string
+
+
+print(to_alternating_case('hello WORLD'))
+print('master YOUR thoughts'.swapcase())
+
+text = "Это был огромный, в два обхвата дуб, с обломанными ветвями и с обломанной корой"
+# разделение по пробелам
+splitted_text = text.split()
+print(splitted_text)
+print(splitted_text[6])  # дуб,
+
+
+def solution(text, ending):
+    return text.endswith(ending)
+
+
+print(solution("text", "xt"))
+
+
+def divisors(n):
+    sol = len([x for x in range(1, n + 1) if n % x == 0])
+    return sol
+
+
+print(divisors(4))
+
+print("".join([str(int(i) ** 2) for i in str(3456)]))
+
+import math
+
+
+def select_subarray(arr):
+    q = []
+    pos = 0
+    while pos != len(arr):
+        to_return_then = arr[pos]
+        arr.remove(arr[pos])
+        if sum(arr) != 0:
+            q1 = round(abs(math.prod(arr) / sum(arr)), 2)
+            q.append(q1)
+        else:
+            q.append(5000000)
+        arr.insert(pos, to_return_then)
+        pos += 1
+        print(q)
+    return [q.index(min(q)), arr[q.index(min(q))]]
+
+
+print(select_subarray([10, 20, -30, 100, 200]))
+
+
+# Напишите функцию, которая будет возвращать количество различных буквенных символов и числовых цифр,
+# не зависящих от регистра, которые встречаются во входной строке более одного раза.
+# Можно предположить, что входная строка содержит только буквы алфавита (как прописные, так и строчные) и числовые цифры.
+# пример: "aA11" -> 2 # 'a' and '1'
+def duplicate_count(text):
+    # chars = []
+    # tex = text.lower()
+    # for i in tex:
+    #     if tex.count(i) > 1 and i not in chars:
+    #         chars.append(i)
+    # return len(chars)
+    return len([c for c in set(text.lower()) if text.lower().count(c) > 1])
+
+
+print(duplicate_count("abcdeaB"))
