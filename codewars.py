f52ef3d1641ff1942f2506cfe3cf654241987c47
titovets.i <titovets.ihar@gmail.com>
2024-09-11 09:13:31 +0300

    codewars задачка

--- codewars.py
+++ codewars.py
@@ -210,3 +210,13 @@ def comp(array1, array2):
 
 print(comp([2, 2, 3], [4, 9, 9]))
 
+# Ваша задача — найти силу гравитации между двумя сферическими объектами (obj1, obj2).
+units = {"kg": 1, "g": 1e-3, "mg": 1e-6, "μg": 1e-9, "lb": 0.453592,
+         "m": 1, "cm": 1e-2, "mm": 1e-3, "μm": 1e-6, "ft": 0.3048,
+         "G": 6.67e-11}
+
+def solution(v, u):
+    m1, m2, r = (v[i] * units[u[i]] for i in range(3))
+    return units["G"] * m1 * m2 / r**2
+
+print(solution([1000, 1000, 100], ["g", "kg", "m"]))
